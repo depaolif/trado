911980af9cb2124d956a9c4ccc5b54fbd6d212a0
Frederico De Paoli <fredericodepaoli@Fredericos-MBP.fios-router.home>
2018-07-01 21:51:59 -0400

    styled a little better, translation disappears on click or scroll

--- contentScript.js
+++ contentScript.js
@@ -1,28 +1,33 @@
-console.log('🐝 running contentscript.js');
-const handleKeyPress = async (evt) => {
+let popup;
+
+const handleKeyPress = (evt) => {
   if (evt.keyCode === 68 && evt.metaKey) {
     evt.preventDefault();
     evt.stopPropagation();
     const selection = window.getSelection();
     const originText = selection.toString();
-    if (selection) {
-      const translation = await getTranslation(originText);
-      const selectionPosition = selection.getRangeAt(0).getBoundingClientRect();
-      displayTranslation(translation, selectionPosition);
-    };
+    if (originText) createTranslation(originText, selection);
   }
-}
+};
+
+const createTranslation = async (originText, selection) => {
+  const translation = await requestTranslation(originText);
+  const selectionPosition = selection.getRangeAt(0).getBoundingClientRect();
+  displayTranslation(translation, selectionPosition);
+  window.addEventListener('click', handleClick);
+  window.addEventListener('scroll', removeListeners);
+};
 
 const displayTranslation = (translation, selectionPosition) => {
   // create popup, text, and arrow elements, assigning respective css
-  const popup = document.createElement('div');
+  popup = document.createElement('div');
   popup.classList.add('trado-wrapper');
   const text = document.createElement('div');
   text.classList.add('trado-text');
   text.innerText = translation;
   const arrow = document.createElement('div');
   arrow.classList.add('trado-arrow');
-  
+
   // calculate position
   const textLeftPos = selectionPosition.left;
   const textRightPos = selectionPosition.right;
@@ -38,6 +43,17 @@ const displayTranslation = (translation, selectionPosition) => {
 
   // append completed node to page
   document.body.appendChild(popup);
-}
+};
+
+const handleClick = (evt) => {
+  if (!evt.target.className.includes('trado')) removeListeners();
+};
+
+const removeListeners = () => {
+  popup.remove();
+  popup = null;
+  window.removeEventListener('click', handleClick);
+  window.removeEventListener('scroll', removeListeners);
+};
 
 window.addEventListener('keydown', handleKeyPress);
--- translationRequest.js
+++ translationRequest.js
@@ -1,15 +1,15 @@
-const subscriptionKey = 'ENTER KEY';
+const subscriptionKey = 'NEW KEY';
 const path = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en';
 const method = 'POST';
 
-const getTranslation = async (text) => {
+const requestTranslation = async (text) => {
   let translation = '';
   const requestBody = [{ Text: text }];
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
   headers.append('Content-Length', text.length);
   headers.append('Ocp-Apim-Subscription-Key', subscriptionKey);
-  let reply = await fetch(path, {
+  const reply = await fetch(path, {
     method,
     headers,
     body: JSON.stringify(requestBody)
