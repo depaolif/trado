b90690c557917c772dd3abe78fb4aa98b34f497f
Frederico De Paoli <fredericodepaoli@Fredericos-MBP.fios-router.home>
2018-07-01 20:13:40 -0400

    adding translation text

--- contentScript.js
+++ contentScript.js
@@ -1,13 +1,26 @@
 console.log('🐝 running contentscript.js');
-window.addEventListener('keydown', (evt) => {
+const handleKeyPress = async (evt) => {
   if (evt.keyCode === 68 && evt.metaKey) {
     evt.preventDefault();
     evt.stopPropagation();
-    const selection = window.getSelection().toString();
-    if (selection) getTranslation(selection);
+    const selection = window.getSelection();
+    const originText = selection.toString();
+    if (selection) {
+      const translation = await getTranslation(originText);
+      const selectionPosition = selection.getRangeAt(0).getBoundingClientRect();
+      displayTranslation(translation, selectionPosition);
+    };
   }
-})
+}
 
-const displayTranslation = (translation) => {
+const displayTranslation = (translation, selectionPosition) => {
+  const popup = document.createElement('div');
+  popup.classList.add('popup');
+  popup.style.left = `${selectionPosition.left}px`;
+  popup.style.top = `${selectionPosition.top - 50}px`;
+  popup.innerText = translation;
+  document.body.appendChild(popup);
   
 }
+
+window.addEventListener('keydown', handleKeyPress);
--- translationRequest.js
+++ translationRequest.js
@@ -1,18 +1,20 @@
-const subscriptionKey = 'ENTER KEY HERE';
+const subscriptionKey = 'ENTER KEY';
 const path = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en';
 const method = 'POST';
 
-const getTranslation = (text) => {
+const getTranslation = async (text) => {
+  let translation = '';
   const requestBody = [{ Text: text }];
   const headers = new Headers();
   headers.append('Content-Type', 'application/json');
   headers.append('Content-Length', text.length);
   headers.append('Ocp-Apim-Subscription-Key', subscriptionKey);
-  fetch(path, {
+  let reply = await fetch(path, {
     method,
     headers,
     body: JSON.stringify(requestBody)
   })
   .then((response) => response.json())
-  .then((response) => displayTranslation(response[0].translations[0].text))
+  .then((response) => response[0].translations[0].text)
+  return reply;
 }
