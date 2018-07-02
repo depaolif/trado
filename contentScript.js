14dae09e434dd260e579e9286a6af44c8ef69852
Frederico De Paoli <fredericodepaoli@Fredericos-MBP.fios-router.home>
2018-07-01 21:19:38 -0400

    cssed correctly

--- contentScript.js
+++ contentScript.js
@@ -14,13 +14,30 @@ const handleKeyPress = async (evt) => {
 }
 
 const displayTranslation = (translation, selectionPosition) => {
+  // create popup, text, and arrow elements, assigning respective css
   const popup = document.createElement('div');
-  popup.classList.add('popup');
-  popup.style.left = `${selectionPosition.left}px`;
-  popup.style.top = `${selectionPosition.top - 50}px`;
-  popup.innerText = translation;
-  document.body.appendChild(popup);
+  popup.classList.add('trado-wrapper');
+  const text = document.createElement('div');
+  text.classList.add('trado-text');
+  text.innerText = translation;
+  const arrow = document.createElement('div');
+  arrow.classList.add('trado-arrow');
   
+  // calculate position
+  const textLeftPos = selectionPosition.left;
+  const textRightPos = selectionPosition.right;
+  const textCenterPos = textLeftPos + ((textRightPos - textLeftPos) / 2);
+  const wrapperCenterPos = textCenterPos - 50;
+
+  popup.style.left = `${wrapperCenterPos}px`;
+  popup.style.top = `${selectionPosition.top - 50}px`;
+
+  // create html element node
+  popup.appendChild(text);
+  popup.appendChild(arrow);
+
+  // append completed node to page
+  document.body.appendChild(popup);
 }
 
 window.addEventListener('keydown', handleKeyPress);
