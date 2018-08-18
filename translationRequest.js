51fcd5e91c7ad952b190fefcf03ea8ea2e2aa0ed
Frederico De Paoli <fredericodepaoli@Fredericos-MBP.fios-router.home>
2018-08-18 14:26:39 -0400

    commented out microsoft api, now using yandex

--- translationRequest.js
+++ translationRequest.js
@@ -1,18 +1,47 @@
-const path = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en';
+// MICROSOFT
+// const path = 'https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=en';
+// YANDEX
+const path = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 const method = 'POST';
 
+// MICROSOFT TRANSLATOR
+// const requestTranslation = async (text) => {
+//   const requestBody = [{ Text: text }];
+//   const headers = new Headers();
+//   headers.append('Content-Type', 'application/json');
+//   headers.append('Content-Length', text.length);
+//   headers.append('Ocp-Apim-Subscription-Key', subscriptionKey);
+//   const reply = await fetch(path, {
+//     method,
+//     headers,
+//     body: JSON.stringify(requestBody)
+//   })
+//   .then((response) => response.json())
+//   .then((response) => response[0].translations[0].text)
+//   return reply;
+// };
+
+// Yandex
+// https://translate.yandex.net/api/v1.5/tr.json/translate
+//  ? key=<API key>
+//  & text=<text to translate>
+//  & lang=<translation direction>
+//  & [format=<text format>]
+//  & [options=<translation options>]
+//  & [callback=<name of the callback function>]
+
 const requestTranslation = async (text) => {
-  const requestBody = [{ Text: text }];
+  text = encodeURI(text);
   const headers = new Headers();
-  headers.append('Content-Type', 'application/json');
+  headers.append('Content-Type', 'application/x-www-form-urlencoded');
   headers.append('Content-Length', text.length);
-  headers.append('Ocp-Apim-Subscription-Key', subscriptionKey);
-  const reply = await fetch(path, {
+  headers.append('Accept', '*/*');
+  const finalPath = `${path}?key=${subscriptionKey}&text=${text}&lang=en`;
+  const reply = await fetch(finalPath, {
     method,
     headers,
-    body: JSON.stringify(requestBody)
   })
   .then((response) => response.json())
-  .then((response) => response[0].translations[0].text)
+  .then((response) => response.text[0]);
   return reply;
-};
+}
